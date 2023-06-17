a2998b62ff1498521c0885e669e790a77bcec32d
harsh23kumar <harshkumarkota09@gmail.com>
2023-06-17 14:26:46 +0530

    Added Generic_rubics_cube_solver model

--- Generic_rubics_cube_solver.h
+++ Generic_rubics_cube_solver.h
@@ -1,14 +1,103 @@
 //
-// Created by DELL on 17-06-2023.
+// Created by Harsh on 17-06-2023.
 //
 
 #ifndef UNTITLED_GENERIC_RUBICS_CUBE_SOLVER_H
 #define UNTITLED_GENERIC_RUBICS_CUBE_SOLVER_H
 
 
-class Generic_rubics_cube_solver {
+#include  "bits/stdc++.h"
+
+using namespace std;
+
+class RubiksCube{
+public:
+    enum class FACE{
+        UP,
+        LEFT,
+        FRONT,
+        RIGHT,
+        BACK,
+        DOWN
+    };
+    enum class COLOR{
+        WHITE,
+        YELLOW,
+        GREEN,
+        BLUE,
+        ORANGE,
+        RED
+    };
+
+    enum class MOVE{
+        L,LPRIME,L2,
+        R,RPRIME,R2,
+        U ,UPRIME, U2,
+        D,DPRIME, D2,
+        F ,FPRIME,F2,
+        B, BPRIME,B2
+    };
+
+    virtual COLOR getColor(FACE face,unsigned row,unsigned col) const=0;
+
+    static char getColorLetter(COLOR color);
+
+    virtual bool isSolved() const=0;
+
+    static string getMove(MOVE ind);
+
+
+    void print() const;
+
+    vector<MOVE> randomshuffleCube(unsigned int times);
+
+    RubiksCube &move(MOVE ind);
+
+    RubiksCube &invert(MOVE ind);
+
+    virtual RubiksCube &f()=0;
+
+    virtual RubiksCube &fPrime()=0;
+
+    virtual RubiksCube &f2()=0;
+
+    virtual RubiksCube &u()=0;
+
+    virtual RubiksCube &uPrime()=0;
+
+    virtual RubiksCube &u2()=0;
+
+    virtual RubiksCube &l()=0;
+
+    virtual RubiksCube &l2()=0;
+
+    virtual RubiksCube &lPrime()=0;
+
+    virtual RubiksCube &r()=0;
+
+    virtual RubiksCube &r2()=0;
+
+    virtual RubiksCube &rPrime()=0;
+
+    virtual RubiksCube &d()=0;
+
+    virtual RubiksCube &d2()=0;
+
+    virtual RubiksCube &dPrime()=0;
+
+    virtual RubiksCube &b()=0;
+
+    virtual RubiksCube &b2()=0;
+
+    virtual RubiksCube &bPrime()=0;
+
+
+    string getCornercolorString(uint8_t) const;
+
+    uint8_t getCornerIndex(uint8_t ind) const;
+    uint8_t getCornerOrientation(uint8_t ind) const;
+
 
 };
-
 
 #endif //UNTITLED_GENERIC_RUBICS_CUBE_SOLVER_H
