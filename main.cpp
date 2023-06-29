4441b843ad853499b13dad8d33718b7c2786e345
harsh23kumar <harshkumarkota09@gmail.com>
2023-06-29 14:46:00 +0530

    Added Generic_rubics_cube_solver model

--- main.cpp
+++ main.cpp
@@ -1,11 +1,27 @@
 #include <bits/stdc++.h>
 #include "Generic_rubics_cube_solver.h"
 #include "RubiksCube3d.cpp"
-#include "RubicksCube1d.cpp"
+#include "DFSSolver.h"
+
 
 using namespace std;
 
 int main() {
+
+    RubiksCube3dArray object3DArray;
+
+    vector<RubiksCube::MOVE> random_shuffleMove=object3DArray.randomShuffleCube(8);
+    for(auto v:random_shuffleMove){
+        cout<<object3DArray.getMove(v)<<" ";
+    }
+    cout<<endl;
+
+    DFSSolver<RubiksCube3dArray,Hash3d> dfssolver(object3DArray,15);
+    vector<RubiksCube::MOVE> movestosolve=dfssolver.solve();
+    dfssolver.rubiksCube.print();
+    for(auto moves:movestosolve) cout<<object3DArray.getMove(moves)<<" ";
+    cout<<endl;
+
 //   RubiksCube3dArray object3DArray;
 //   object3DArray.print();
 //   object3DArray.b();
@@ -16,10 +32,10 @@ int main() {
 //      cout<<object3DArray.getMove(v)<<" ";
 //  }
 
-RubiksCube1dArray object1DArray;
-object1DArray.print();
-object1DArray.lPrime();
-    object1DArray.print();
+//RubiksCube1dArray object1DArray;
+//object1DArray.print();
+//object1DArray.lPrime();
+//    object1DArray.print();
 
     return 0;
 }
