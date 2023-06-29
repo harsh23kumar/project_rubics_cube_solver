2e0483a3b80517b315a838e910114deb4110a5a3
harsh23kumar <harshkumarkota09@gmail.com>
2023-06-29 13:29:07 +0530

    Added Generic_rubics_cube_solver model

--- main.cpp
+++ main.cpp
@@ -1,13 +1,25 @@
 #include <bits/stdc++.h>
 #include "Generic_rubics_cube_solver.h"
 #include "RubiksCube3d.cpp"
+#include "RubicksCube1d.cpp"
+
 using namespace std;
 
 int main() {
-   RubiksCube3dArray object3DArray;
-   object3DArray.print();
-   object3DArray.b();
+//   RubiksCube3dArray object3DArray;
+//   object3DArray.print();
+//   object3DArray.b();
+//
+//vector<RubiksCube::MOVE> arr=object3DArray.randomShuffleCube(4);
+//
+//  for(auto v:arr){
+//      cout<<object3DArray.getMove(v)<<" ";
+//  }
 
+RubiksCube1dArray object1DArray;
+object1DArray.print();
+object1DArray.lPrime();
+    object1DArray.print();
 
     return 0;
 }
